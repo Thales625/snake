0612ad6254b7e4a2fa4de97eb56c93206a14b03d
Thales Rodrigues <thaleshend@gmail.com>
2025-03-16 13:10:53 -0300

    working! todo: random apple generation

--- snake.asm
+++ snake.asm
@@ -11,23 +11,22 @@
 .eqv NONE $s0
 .eqv COLOR $s1
 
-.eqv DISPLAY_PTR $s2
-.eqv DISPLAY_START_ADDR $s3
+.eqv DISPLAY_START_ADDR $s2
+.eqv DISPLAY_PTR $t0
+.eqv DISPLAY_X $t1
+.eqv DISPLAY_Y $t2
 
-.eqv STACK_START_ADDR $s4
-.eqv STACK_HEAD_PTR $s5 # snake head
+.eqv STACK_START_ADDR $s3
+.eqv STACK_HEAD_PTR $s4 # snake head
 
-.eqv HEAD_NEXT_X $t8
-.eqv HEAD_NEXT_Y $t9
-
-.eqv AUX_X $t2
-.eqv AUX_Y $t3
-
-.eqv APPLE_X $t2
-.eqv APPLE_Y $t3
+.eqv APPLE_X $s5
+.eqv APPLE_Y $s6
 
 .eqv MOVE_X $t4
 .eqv MOVE_Y $t5
+
+.eqv HEAD_NEXT_X $t6
+.eqv HEAD_NEXT_Y $t7
 
 # CONSTS
 .eqv WIDTH 64
@@ -41,91 +40,78 @@
 # MACROS
 .include "utils.asm"
 
-# TODO: transform in subroutine
-.macro display_addr(%x, %y) # set DISPLAY_PTR
-	# li $t8, %y
-	move $t8, %y
-	mul $t8, $t8, WIDTH
-	add $t8, $t8, %x
-	sll $t8, $t8, 2
-	add DISPLAY_PTR, $t8, DISPLAY_START_ADDR
-.end_macro
-
-.macro plot
-	sw COLOR, 0(DISPLAY_PTR)
-.end_macro
-
 .macro set_color(%color)
 	addi COLOR, $zero, %color
-	# li COLOR, %color
 .end_macro
 
 .macro shift_stack
-	# $t6 -> i
-	# $t7 -> aux
-	move $t6, STACK_START_ADDR
+	# $t0 -> i
+	# $t1 -> aux
+	move $t0, STACK_START_ADDR
 	loop_shift_stack:
-	lw $t7, 8($t6)
-	sw $t7, 0($t6)
+	lw $t1, 8($t0)
+	sw $t1, 0($t0)
 
-	lw $t7, 12($t6)
-	sw $t7, 4($t6)
+	lw $t1, 12($t0)
+	sw $t1, 4($t0)
 
-	addi $t6, $t6, 8
+	addi $t0, $t0, 8
 
-	blt $t6, STACK_HEAD_PTR, loop_shift_stack
+	blt $t0, STACK_HEAD_PTR, loop_shift_stack
 .end_macro
 
 .macro update
-	lw HEAD_NEXT_X, (STACK_HEAD_PTR)
+	lw HEAD_NEXT_X, (STACK_HEAD_PTR) # head_next_x
 	add HEAD_NEXT_X, HEAD_NEXT_X, MOVE_X
 
-	lw HEAD_NEXT_Y, 4(STACK_HEAD_PTR)
+	lw HEAD_NEXT_Y, 4(STACK_HEAD_PTR) # head_next_y
 	add HEAD_NEXT_Y, HEAD_NEXT_Y, MOVE_Y
 
-	# ignore apple collision for now
+	# plot snake head
+	set_color(GREEN)
+	move DISPLAY_X, HEAD_NEXT_X
+	move DISPLAY_Y, HEAD_NEXT_Y
+	jal plot
+
+	beq APPLE_X, HEAD_NEXT_X, update_check_y # apply_x ==  head_next_x
+	j update_case_not_eat
+	update_check_y:
+	beq APPLE_Y, HEAD_NEXT_Y, update_case_eat # apply_y ==  head_next_y
+	j update_case_not_eat
+	# CASE EAT
+	update_case_eat:
+		print_int(APPLE_Y) # DEBUG
+
+		addi STACK_HEAD_PTR, STACK_HEAD_PTR, 8 # increase STACK_HEAD_PTR
+		# update apple position
+		addi APPLE_X, $zero, 3
+		addi APPLE_Y, $zero, 16
+		# plot apple
+		set_color(RED)
+		move DISPLAY_X, APPLE_X
+		move DISPLAY_Y, APPLE_Y
+		jal plot
+
+		j update_end
+
+	# CASE NOT EAT
+	update_case_not_eat:
+		# print_int(APPLE_X) # DEBUG
+		# clear shifted point (last snake tail)
+		set_color(BACKGROUND)
+		lw DISPLAY_X, (STACK_START_ADDR)  # x
+		lw DISPLAY_Y, 4(STACK_START_ADDR) # y
+		jal plot
+
+		shift_stack()
 	
-	lw $t6, (STACK_START_ADDR)  # x
-	lw $t7, 4(STACK_START_ADDR) # y
-	set_color(BACKGROUND)
-	# set_color(RED)
-	display_addr($t6, $t7)
-	plot()
+		j update_end
 
-	shift_stack()
-
+	update_end:
+	# set next snake head position
 	sw HEAD_NEXT_X, (STACK_HEAD_PTR)
 	sw HEAD_NEXT_Y, 4(STACK_HEAD_PTR)
-.end_macro
 
-.macro setup_memory
-	# mem[stack[2*head_ptr+1]][stack[2*head_ptr]] = CHAR_HEAD;
-	# lw AUX_X, (STACK_HEAD_PTR)
-	# lw AUX_Y, 4(STACK_HEAD_PTR)
-
-	set_color(GREEN)
-	display_addr(AUX_X, AUX_Y)
-	plot()
-
-	# $t6 -> i
-	move $t6, STACK_START_ADDR
-	loop_setup_memory:
-	lw AUX_X, 0($t6) # x
-	lw AUX_Y, 4($t6) # y
-
-	# if AUX_X != -1
-	bne AUX_X, NONE, true_setup_memory
-	bne AUX_Y, NONE, true_setup_memory
-	j false_setup_memory
-
-	true_setup_memory:
-	# set_color(GREEN)
-	display_addr(AUX_X, AUX_Y)
-	plot()
-	false_setup_memory:
-	addi $t6, $t6, 8
-
-	blt $t6, STACK_HEAD_PTR, loop_setup_memory
 .end_macro
 
 .data
@@ -173,18 +159,34 @@ addi NONE, $zero, -1
 	sw $t0, 4(STACK_HEAD_PTR)
 	
 	# apple
-	addi $zero, APPLE_X, 3
-	addi $zero, APPLE_Y, 6
+	addi APPLE_X, $zero, 3
+	addi APPLE_Y, $zero, 6
+
+	# plot apple
+	set_color(RED)
+	move DISPLAY_X, APPLE_X
+	move DISPLAY_Y, APPLE_Y
+	jal plot
 
 	# move
-	addi $zero, MOVE_X, 1
-	addi $zero, MOVE_Y, 0
+	addi MOVE_X, $zero, 0
+	addi MOVE_Y, $zero, 1
 # END
 
 loop:
-sleep(1000)
-update()
-setup_memory()
-j loop
+	sleep(1000)
+	update()
+	j loop
+
+# subroutines
+j end
+plot:
+	mul DISPLAY_Y, DISPLAY_Y, WIDTH
+	add DISPLAY_Y, DISPLAY_Y, DISPLAY_X
+	sll DISPLAY_Y, DISPLAY_Y, 2
+	add DISPLAY_PTR, DISPLAY_Y, DISPLAY_START_ADDR
+
+	sw COLOR, 0(DISPLAY_PTR)
+	jr $ra
 
 end: done()
