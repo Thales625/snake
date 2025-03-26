cea9b0a96b0a25120295d8c69b80b9cb5ca16946
Thales Rodrigues <thaleshend@gmail.com>
2025-03-25 21:05:31 -0300

    update game-over image & fix main.py

--- image/main.py
+++ image/main.py
@@ -1,5 +1,4 @@
 from PIL import Image
-import PIL
 import os
 
 directory = os.path.dirname(os.path.abspath(__file__))
@@ -10,26 +9,9 @@ for filename in os.listdir(directory):
 		asm_path = os.path.join(directory, f'{os.path.splitext(filename)[0]}.asm')
 
 		with Image.open(img_path) as img:
-			if False: # RESIZE
-				TAM = 225
-				dx = 30
-				dy = 30
-
-				# dn = 225/2 - 64
-
-				dx = dy = TAM/2 - 64
-
-				left = dx
-				top = dy
-				right = TAM - dx
-				bottom = TAM - dy
-				img = img.crop((left, top, right, bottom))
+			print(img_path)
 
 			img = img.resize((64, 64))
-			# img = img.resize((64, 64), resample=PIL.Image.LANCZOS)
-			# img = img.resize((64, 64), resample=PIL.Image.BILINEAR)
-			# img = img.resize((64, 64), resample=PIL.Image.BICUBIC)
-			# img = img.resize((64, 64), resample=PIL.Image.NEAREST)
 			
 			img = img.transpose(Image.ROTATE_90)
 			img = img.transpose(Image.FLIP_LEFT_RIGHT)
@@ -39,7 +21,12 @@ for filename in os.listdir(directory):
 			for x in range(img.width):
 				for y in range(img.height):
 					pixel = img.getpixel((x, y))
-					pixels.append(f'0x00{pixel[0]:02X}{pixel[1]:02X}{pixel[2]:02X}')
+
+					if isinstance(pixel, tuple):
+						pixels.append(f'0x00{pixel[0]:02X}{pixel[1]:02X}{pixel[2]:02X}')
+					else:
+						print("Image is not in multi-layer colors")
+						exit()
 
 			with open(asm_path, 'w') as f:
 				f.write(".word " + ",".join(pixels[::-1]))
